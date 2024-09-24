d25abae835deb79d5b2049414523451fda48f42f
Chamir <114265988+sarcosmos@users.noreply.github.com>
2024-09-23 22:58:59 -0500

    Add project files.

---
+++ Document Management_Backend/Context/DMDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Document_Management_Backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Document_Management_Backend.Context
+{
+    public class DMDbContext : DbContext
+    {
+
+        public DMDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
+
+        }
+    }
+} 
---
+++ Document Management_Backend/Model/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Document_Management_Backend.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string  Password { get; set; }
+
+    }
+}
