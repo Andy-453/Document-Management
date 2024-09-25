20aad9a96920e3d7b8527a76da9936def009a85e
yef-max <boltex996@gmail.com>
2024-09-24 19:57:51 -0500

    todas las tablas

---
+++ Document Management_Backend/Model/Document_has_history.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Document_Management_Backend.Model
+{
+    public class Documents_has_history
+    {
+        public int Documents_Document_Id { get; set; }
+
+        public int historic_Historic_Id { get; set; }
+    }
+}
