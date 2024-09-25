9092f9fce967d38dab262a29d0bb4c6e77f8ae50
yef-max <boltex996@gmail.com>
2024-09-24 19:28:32 -0500

    tabla de family_group

---
+++ Document Management_Backend/Model/Family_Group.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Document_Management_Backend.Model
+{
+    public class Family_Group
+    {
+        public int Users_UserType_Id { get; set; }
+
+        public int Documents_Document_Id { get; set; }
+
+        public required string Members { get; set; }
+    }
+}
