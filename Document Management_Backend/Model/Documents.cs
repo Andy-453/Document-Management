f18de828cfb9b89a37cd1eb9e66d18215b84f013
yef-max <boltex996@gmail.com>
2024-09-24 19:51:47 -0500

    tabla documentos

---
+++ Document Management_Backend/Model/Documents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+    public class Documents
+    {
+        public int Document_Id { get; set; }
+
+        public required int Documents_Protected { get; set; }
+
+        public required int Document_shared { get; set; }
+
+        public required string Address { get; set; }
+
+        public required string Type_Document { get; set; }
+    }
+}
